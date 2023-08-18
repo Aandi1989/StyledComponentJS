f32104fb8a643acce0cbe37918d935544fdf848e
Aandi1989 <navaliny1989@gmail.com>
2023-08-18 18:25:57 +0300

    pseudo-classes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
-import Title from './components/Title';
-import Flex from './components/Flex';
-import Console from './components/Console';
-
+import React from "react";
+import styled from "styled-components";
+import Title from "./components/Title";
+import Flex from "./components/Flex";
+import Console from "./components/Console";
+import Button from "./components/Button";
 
 const AppWrapper = styled.div`
-width:100%;
-min-height:100vh;
-padding:2rem;
-background:black;
-`
+  width: 100%;
+  min-height: 100vh;
+  padding: 2rem;
+  background: black;
+`;
 
 function App() {
   return (
     <AppWrapper>
-      <Flex justify={'center'}>
-        <Title color={'green'}>Console cmd 20223. 18 August</Title>
+      <Flex justify={"center"}>
+        <Title color={"green"}>Console cmd 20223. 18 August</Title>
       </Flex>
-      <Console/>
+      <Flex direction="column">
+        <Console />
+        <Button align='flex-end'>Отправить</Button>
+      </Flex>
     </AppWrapper>
   );
 }
