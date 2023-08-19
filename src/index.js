e1213b9c9b0a1935ff62ab1003f6c7e04c4e2641
Aandi1989 <navaliny1989@gmail.com>
2023-08-19 10:06:35 +0300

    media queries

--- src/index.js
+++ src/index.js
@@ -16,6 +16,11 @@ const theme = {
   colors:{
     primary:'red',
     secondary:'green',
+  },
+  media:{
+    phone:"(max-width: 425px)",
+    tablet:"(max-width: 768px) and (min-width: 425px)",
+
   }
 }
 
